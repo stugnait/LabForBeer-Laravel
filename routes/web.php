f5b2e09c08af86e280432992be8f4e2ad49b7c71
Igor Teslitskii <i.teslitskii@gmail.com>
2024-03-02 17:29:00 +0200

    Logout added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,14 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware(['auth'])->name('dashboard');
+
+
+
+Route::controller(AuthenticatedSessionController::class)->group(function(){
+    Route::get('logout','destroy')->name('logout');
+});
+
+
+
 
 require __DIR__.'/auth.php';
